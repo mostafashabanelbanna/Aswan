3c02fe32380af76a42f46fccc297e016d4865b39
Mostafa Shaban <moshaban@SH-MOSTAFA-SHAB>
2021-07-28 15:08:16 +0200

    famous slick slider

--- src/components/government-famous-component.js
+++ src/components/government-famous-component.js
@@ -1,45 +1,98 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Slider from "react-slick";
 
 function GovernmentFamous(props) {
+  var settings = {
+    dots: false,
+    arrows: false,
+    // autoplay:true,
+    // autoplaySpeed:1000,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="container-fluid col-11 mt-5 mb-2">
+    <div className="container mt-5 mb-2">
       <div className="d-flex mb-3 justify-content-center justify-content-md-start">
         <FontAwesomeIcon icon={faStar} className="titleIcon" />
         <p className="titles mb-1">مشاهير المحافظة</p>
       </div>
-      <div className="d-flex flex-lg-row justify-content-lg-between">
-        <div>
+      <Slider {...settings}>
+        <div className="d-flex flex-column align-items-center justify-content-center">
           <div className="outerPolygon">
             <div className="innerPolygon"></div>
           </div>
           <p className="text-center">محمد منير</p>
         </div>
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
           <div className="outerPolygon">
             <div className="innerPolygon"></div>
           </div>
           <p className="text-center">محمد منير</p>
         </div>
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
           <div className="outerPolygon">
             <div className="innerPolygon"></div>
           </div>
           <p className="text-center">محمد منير</p>
         </div>
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
           <div className="outerPolygon">
             <div className="innerPolygon"></div>
           </div>
           <p className="text-center">محمد منير</p>
         </div>
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
           <div className="outerPolygon">
             <div className="innerPolygon"></div>
           </div>
           <p className="text-center">محمد منير</p>
         </div>
-      </div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <div className="outerPolygon">
+            <div className="innerPolygon"></div>
+          </div>
+          <p className="text-center">محمد منير</p>
+        </div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <div className="outerPolygon">
+            <div className="innerPolygon"></div>
+          </div>
+          <p className="text-center">محمد منير</p>
+        </div>
+      </Slider>
+
       <div className="line mx-auto my-5 w-100"></div>
     </div>
   );
