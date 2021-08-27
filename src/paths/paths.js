c2795cfb95d11378ae05112dcacd7f62fb424d53
Mamdouh Nageh <76902725+MamdohNageh@users.noreply.github.com>
2021-08-27 14:35:59 +0200

    merge

--- src/paths/paths.js
+++ src/paths/paths.js
@@ -1,10 +1,12 @@
 const MainPath = "http://41.128.217.181:10090/upload/";
+
 // const MainPath = "/images/Upload/";
 
 export const paths = {
   ProjectPhoto: `${MainPath}NationalProject/Photo/`,
   ProjectAttachment: `${MainPath}NationalProject/Attachment/`,
-  ProjectPhotos:`${MainPath}PhotoLibraryPhoto/Photo/`,
+  ProjectPhotos: `${MainPath}PhotoLibraryPhoto/Photo/`,
   PhotoLibraryAlbum: `${MainPath}PhotoLibrary/Photo/`,
-  ImportantPortalsPhotos: `${MainPath}WebLink/Photo/`
+  ImportantPortalsPhotos: `${MainPath}WebLink/Photo/`,
+  NewsPhotos: `${MainPath}News/Photo/`,
 };
