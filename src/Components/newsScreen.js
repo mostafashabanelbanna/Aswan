843ba987d81c5b648895e1960e6de26b87575576
Mostafa Shaban <moshaban@SH-MOSTAFA-SHAB>
2021-08-27 16:25:32 +0200

    merge conflict

--- src/Components/newsScreen.js
+++ src/Components/newsScreen.js
@@ -19,79 +19,6 @@ const News = (props) => {
     props.mainNews();
   }, []);
 
-<<<<<<< HEAD
-    useEffect(() => {
-        props.sliderNews();
-        props.mainNews();
-    }, [])
-
-    var settings = {
-        dots: false,
-        // autoplay:true,
-        // autoplaySpeed:1000,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    arrows: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-    if (props.mainews && props.news)
-        return (
-            <div>
-                <div className="container p-0">
-                    <div className="m-3 d-flex">
-                        <img src="/images/icons/news_titel-0١.png" width='50px' />
-                    <div className='underline'>   <h3 className="mt-4 me-2 text-secondary"> اخبار المحافظة </h3></div>
-                    </div>
-                    <div className="row p-0 mt-3">
-                        <div className=" text-secondary">
-                            <h2 className="mb-lg-5 mb-2">
-                                {props.mainews.result.title}
-                            </h2>
-                        </div>
-                        <div className=" col-12 col-sm-12 d-flex d-lg-block align-items-center align-content-center  p-2 lh-lg text-secondary order-sm-2 order-lg-1 order-2">
-                            <div>
-                                <div className=" float-lg-start  col-lg-6 me-sm-0 me-md-0  me-lg-5   col-sm-12 col-12 d-flex d-lg-block justify-content-center align-items-center align-content-center order-lg-2  order-sm-1 order-1">
-                                    <img
-                                        className="  mt-3 mb-3 rounded-3 w-100"
-                                        src={'images/Upload/News/Photo/' + props.mainews.result.id + '/' + props.mainews.result.photo}
-                                        alt={props.mainews.result.title}
-                                    />
-                                </div>
-                               <div id='newscontent'> {ReactHtmlParser(props.mainews.result.content)}</div>
-                            </div>
-
-                        </div>
-
-                    </div>
-=======
   var settings = {
     dots: false,
     // autoplay:true,
@@ -158,7 +85,6 @@ const News = (props) => {
                     {" "}
                     {ReactHtmlParser(props.mainews.result.content)}
                   </div>
->>>>>>> c2795cfb95d11378ae05112dcacd7f62fb424d53
                 </div>
               </div>
             </div>
