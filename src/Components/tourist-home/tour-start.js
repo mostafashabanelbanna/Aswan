d30be73da4b8a4ce67d93d18deef847669ddbb5d
mahmoud <mahmoud.mfarag3@gmail.com>
2021-09-05 10:56:48 +0200

    EServices

--- src/Components/tourist-home/tour-start.js
+++ src/Components/tourist-home/tour-start.js
@@ -5,9 +5,11 @@ const TourStart =()=>{
 
     var settings = {
         dots: false,
-        // autoplay:true,
-        // autoplaySpeed:1000,
-        infinite: false,
+        arrows:false,
+        centerMode:true,
+      infinite: true,
+      centerPadding: "80px",
+
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -16,19 +18,18 @@ const TourStart =()=>{
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: false
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 800,
                 settings: {
                     arrows: false,
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
-                    initialSlide: 2
                 }
             },
             {
