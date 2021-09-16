28465314e2f44a76bd2f322e8a52940f85720f5b
Mamdouh Nageh <76902725+MamdohNageh@users.noreply.github.com>
2021-09-16 08:32:18 +0200

    edit

--- src/Components/home-page/advertisment/advertisment-list.js
+++ src/Components/home-page/advertisment/advertisment-list.js
@@ -148,7 +148,7 @@ const AdvertismentList = (props) => {
                       <ListWithImage
                         imgSrc={paths.ads + item.id + "/" + item.photo}
                         title={item.title}
-                        date={"1/1/2022"}
+                        date={`${moment(new Date(item.publishDate)).format("LL")}`}
                         category={item.advertismentTypeName}
                         imgHeight="200px"
                       />
