b99b80df1371cb358f4e87c6a36299a3fa3489df
Mostafa Shaban <moshaban@SH-MOSTAFA-SHAB>
2021-11-11 15:25:31 +0200

    audio

--- src/Components/ui/react-audio-player.js
+++ src/Components/ui/react-audio-player.js
@@ -1,77 +1,94 @@
-// import ReactAudioPlayer from "react-audio-player";
+import React, { useState, useEffect } from "react";
 
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(true);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = () => {
+  const url = "/audio/160547144-epic-egyptian-middle-eastern-a.wav";
+  const [playing, toggle] = useAudio(url);
+
+  return (
+    <div>
+      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+    </div>
+  );
+};
+
+export default Player;
+
+// import useSound from "use-sound";
+// import { Button } from "react-bootstrap";
+// import React, { useState, useEffect } from "react";
 // const AudioPlayer = () => {
+//   const soundUrl = "/audio/160547144-epic-egyptian-middle-eastern-a.wav";
+
+//   const [play, { stop }] = useSound(soundUrl, { volume: 0.5 });
+
+//   const [isHovering, setIsHovering] = React.useState(false);
+
+//   window.onload = play();
 //   return (
-//     <ReactAudioPlayer
-//       src="/audio/160547144-epic-egyptian-middle-eastern-a.wav"
-//       autoPlay
-//       controls
-//       loop
-//     />
+//     <Button
+//       onMouseEnter={() => {
+//         setIsHovering(true);
+//         play();
+//       }}
+//       onMouseLeave={() => {
+//         setIsHovering(false);
+//         stop();
+//       }}
+//       isHovering={isHovering}
+//     >
+//       Hover over me
+//     </Button>
 //   );
 // };
 
 // export default AudioPlayer;
 
-import useSound from "use-sound";
-import { Button } from "react-bootstrap";
-import React, { useState, useEffect } from "react";
-const AudioPlayer = () => {
-  const soundUrl = "/audio/160547144-epic-egyptian-middle-eastern-a.wav";
+// import React, { useState, useEffect } from "react";
 
-  const [play, { stop }] = useSound(soundUrl, { volume: 0.5 });
-
-  const [isHovering, setIsHovering] = React.useState(false);
-
-  window.onload = play();
-  return (
-    <Button
-      onMouseEnter={() => {
-        setIsHovering(true);
-        play();
-      }}
-      onMouseLeave={() => {
-        setIsHovering(false);
-        stop();
-      }}
-      isHovering={isHovering}
-    >
-      Hover over me
-    </Button>
-  );
-};
-
-export default AudioPlayer;
-
-// const useAudio = (url) => {
+// const useAudio = url => {
 //   const [audio] = useState(new Audio(url));
-//   const [playing, setPlaying] = useState(true);
+//   const [playing, setPlaying] = useState(false);
 
 //   const toggle = () => setPlaying(!playing);
 
 //   useEffect(() => {
-//     const resolver = async () => {
-//       await audio.play();
+//       playing ? audio.play() : audio.pause();
+//     },
+//     [playing]
+//   );
+
+//   useEffect(() => {
+//     audio.addEventListener('ended', () => setPlaying(false));
+//     return () => {
+//       audio.removeEventListener('ended', () => setPlaying(false));
 //     };
-
-//     resolver();
-//     // playing ? audio.play() : audio.pause();
 //   }, []);
-
-//   //   useEffect(() => {
-//   //     audio.addEventListener("ended", () => setPlaying(false));
-//   //     return () => {
-//   //       audio.removeEventListener("ended", () => setPlaying(false));
-//   //     };
-//   //   }, []);
 
 //   return [playing, toggle];
 // };
 
 // const Player = ({ url }) => {
-//   const [playing, toggle] = useAudio(
-//     "/audio/160547144-epic-egyptian-middle-eastern-a.wav"
-//   );
+//   const [playing, toggle] = useAudio(url);
 
 //   return (
 //     <div>
@@ -81,39 +98,3 @@ export default AudioPlayer;
 // };
 
 // export default Player;
-
-// import React, { useEffect, useState } from "react";
-// import { useAudioPlayer } from "react-use-audio-player";
-// import { AudioPlayerProvider } from "react-use-audio-player";
-
-// const AudioPlayer = ({ file }) => {
-//   const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
-//     src: file,
-//     format: "wav",
-//     autoplay: true,
-//     html5: true,
-//   });
-
-//   useEffect(() => {
-//     togglePlayPause();
-//   }, []);
-
-//   if (!ready && !loading) return <div>No audio to play</div>;
-//   if (loading) return <div>Loading audio</div>;
-
-//   return (
-//     <div>
-//       <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
-//     </div>
-//   );
-// };
-
-// const AudioPlayerProv = () => {
-//   return (
-//     <AudioPlayerProvider>
-//       <AudioPlayer file="/audio/160547144-epic-egyptian-middle-eastern-a.wav" />
-//     </AudioPlayerProvider>
-//   );
-// };
-
-// export default AudioPlayerProv;
