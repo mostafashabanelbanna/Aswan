148f69ac446a6d6d33227a48b17db3e0b280db0a
Mamdouh Nageh <76902725+MamdohNageh@users.noreply.github.com>
2022-03-10 10:39:38 +0200

    ticker

--- src/Components/news-ticker.js
+++ src/Components/news-ticker.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import axios from "../Axios/Axios_Config";
 import { Link } from "react-router-dom";
+
 // const GetNewsFromAPI = () => {
 // let News;
 
@@ -17,39 +18,91 @@ import { Link } from "react-router-dom";
 //     );
 //   };
 
-function StockTicker() {
-  const [News, setNews] = useState("");
-  const [arr, setArr] = useState([]);
-  async function fetchData() {
-    let res = await axios.get("/AdvertismentAPI/GetFocus");
-    if (res && res.data) {
-      return res.data.result;
+// function StockTicker() {
+//   const [News, setNews] = useState("");
+//   const [arr, setArr] = useState([]);
+//   async function fetchData() {
+//     let res = await axios.get("/AdvertismentAPI/GetFocus");
+//     if (res && res.data) {
+//       return res.data.result;
+//     }
+//   }
+//   useEffect(async () => {
+//     let x = await fetchData();
+//     setArr(arr.length ? [...arr, x] : [x]);
+//   }, []);
+
+//   console.log("arr", arr);
+
+//   let titles = [];
+//   for (let i = 0; i < arr?.length; i++) {
+//     titles.push(arr[i]?.title);
+//   }
+//   if (arr.length > 0 && arr !== []) {
+//     return (
+//       <Ticker move={true} direction={"toRight"} speed={5} mode={"smooth"}>
+//         {() =>
+//           arr ? (
+//             <Link to={`/advertisment-details/`} className="text-dark">
+//               <p className="my-2" style={{ whiteSpace: "nowrap" }}>
+//                 {" "}
+//                 {titles.join(" ...")} ...&nbsp;&nbsp;&nbsp;&nbsp;
+//               </p>
+//             </Link>
+//           ) : null
+//         }
+//       </Ticker>
+//     );
+//   } else return <div style={{ visibility: "hidden" }}></div>;
+// }
+
+// export default StockTicker;
+
+const GetNewsFromAPI = ({ index }) => {
+  const [news, setNews] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const ratesFromAPI = await axios.get("/AdvertismentAPI/GetFocus");
+      setNews(ratesFromAPI?.data?.result);
     }
-  }
-  useEffect(async () => {
-    let x = await fetchData();
-    setArr(arr.length ? [...arr, x] : [x]);
+    fetchData();
   }, []);
 
-  if (arr.length > 0 && arr !== []) {
-    return (
-      <Ticker move={true} direction={"toRight"} speed={5} mode={"smooth"}>
-        {() =>
-          arr[0].title ? (
-            <Link
-              to={`/advertisment-details/${arr[0].id}`}
-              className="text-dark"
-            >
-              <p className="my-2" style={{ whiteSpace: "nowrap" }}>
-                {" "}
-                {arr[0].title} ...&nbsp;&nbsp;&nbsp;&nbsp;
-              </p>
-            </Link>
-          ) : null
-        }
-      </Ticker>
-    );
-  } else return <div style={{ visibility: "hidden" }}></div>;
+  console.log("news", news);
+  // A placeholder is needed, to tell react-ticker, that width and height might have changed
+  // It uses MutationObserver internally
+  return news ? (
+    <p style={{ whiteSpace: "nowrap" }}>
+      {news.map((item) => {
+        return (
+          <Link
+            key={item.id}
+            to={`/advertisment-details/${item.id}`}
+            className="text-dark"
+          >
+            {item.title}
+            ...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </Link>
+        );
+      })}
+    </p>
+  ) : (
+    <p style={{ visibility: "hidden" }}>placeholder</p>
+  );
+};
+
+function StockTicker() {
+  return (
+    <Ticker
+      // move={true}
+      // direction={"toRight"}
+      speed={5}
+      // mode={"smooth"}
+      offset="run-in"
+    >
+      {({ index }) => <GetNewsFromAPI index={index} />}
+    </Ticker>
+  );
 }
 
 export default StockTicker;
